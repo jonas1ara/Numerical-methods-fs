617989dfdec5388a10c9a021947a04b2f4c0952d
Jonas Lara <adrianlr2515@outlook.com>
2023-11-16 01:01:47 -0600

    Update

--- src/Newton-Raphson-Method/Program.fs
+++ src/Newton-Raphson-Method/Program.fs
@@ -1,22 +1,17 @@
-﻿let newtonRaphson (f: float -> float) (fPrime: float -> float) (initialGuess: float) (tolerance: float) (maxIterations: int) =
-    let rec iterate x n =
+﻿let newtonRaphson (f: float -> float) (df: float -> float) (x0: float) (tol: float) (maxIter: int) =
+    let rec loop (x: float) (iter: int) =
         let fx = f x
-        if abs fx < tolerance || n = maxIterations then
-            x
-        else
-            let xNext = x - fx / (fPrime x)
-            iterate xNext (n + 1)
+        let dfx = df x
+        let x1 = x - fx / dfx
+        if abs (x1 - x) < tol then x1
+        elif iter < maxIter then loop x1 (iter + 1)
+        else failwith "Newton-Raphson method failed to converge"
+    loop x0 0
 
-    iterate initialGuess 0
 
-// Example usage:
-let f x = x * x - 2.0  // Function for which we want to find the root (e.g., sqrt(2))
-let fPrime x = 2.0 * x // Derivative of the function
+let f x = x ** 3.0 - 2.0 * x - 5.0
+let df x = 3.0 * x ** 2.0 - 2.0
 
-let initialGuess = 1.0 // Initial guess for the root
-let tolerance = 1e-6   // Tolerance for stopping criterion
-let maxIterations = 100 // Maximum number of iterations
-
-let result = newtonRaphson f fPrime initialGuess tolerance maxIterations
-printfn "Approximate root: %f" result
+let root = newtonRaphson f df 1.0 0.0001 1000
+printfn "The root of the function is %f" root
 
