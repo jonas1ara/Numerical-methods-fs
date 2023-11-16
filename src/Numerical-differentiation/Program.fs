3c36b95156f1833514715ea7031e8dcc2d9cb29f
Jonas Lara <adrianlr2515@outlook.com>
2023-11-16 00:26:41 -0600

    update

---
+++ src/Numerical-differentiation/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let diff (f: float -> float) (x: float) (h: float) =
+    (f (x + h) - f (x - h)) / (2.0 * h)
+
+let f x = sin x
+let x = 0.0
+let h = 0.1
+let df = diff f x h
+
+printfn "The first derivative of sin(0) is approximately %f" df
+
+
---
+++ src/git.sh
@@ -0,0 +1,9 @@
+git pull
+
+git add .
+
+git commit -m "update"
+
+git push -u  origin main
+
+
