f1b8b89a2ae3e05ae21a5b8a546cba319c6fbc90
Jonas Lara <adrianlr2515@outlook.com>
2024-01-21 23:59:47 -0600

    Bisection-method

--- src/Bisection-method/Program.fs
+++ src/Bisection-method/Program.fs
@@ -1,2 +1,23 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+let epsilon = 1e-6
+
+let rec bisection (f: float -> float) (a: float) (b: float) =
+    let fa = f a
+    let fb = f b
+    if abs (b - a) < epsilon then
+        (a + b) / 2.0
+    else
+        let c = (a + b) / 2.0
+        let fc = f c
+        if fa * fc < 0.0 then
+            bisection f a c
+        else
+            bisection f c b
+
+let f x = x * x - 2.0 // Función de ejemplo: x^2 - 2
+let a = 1.0
+let b = 2.0
+let root = bisection f a b
+
+printfn "The root of the function is approximately %f" root
