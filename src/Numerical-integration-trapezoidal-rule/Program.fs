0e1a1e7ec3cc7000326e9ee5c3f478073e3a4988
Jonas Lara <adrianlr2515@outlook.com>
2024-01-23 22:52:28 -0600

    Added Numerical integration trapezoidal rule

--- src/Numerical-integration-trapezoidal-rule/Program.fs
+++ src/Numerical-integration-trapezoidal-rule/Program.fs
@@ -1,2 +1,16 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+let trapezoidalRule (f: float -> float) (a: float) (b: float) (n: int) =
+    let h = (b - a) / float n
+    let sum = 
+        seq { for i in 1 .. n-1 -> a + float i * h }
+        |> Seq.sumBy (fun xi -> f xi)
+    0.5 * h * (f a + 2.0 * sum + f b)
+
+let f x = sin x
+let a = 0.0
+let b = 1.0
+let n = 100
+let integral = trapezoidalRule f a b n
+
+printfn "The numerical integral of sin(x) from %f to %f is approximately %f" a b integral
