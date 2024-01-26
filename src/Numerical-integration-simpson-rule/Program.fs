5740dcacb387eb7a1f79b480b9773fcdedf5b110
Jonas Lara <adrianlr2515@outlook.com>
2024-01-25 20:24:47 -0600

    Added Numerical-simpson-rectangle-rule

--- src/Numerical-integration-simpson-rule/Program.fs
+++ src/Numerical-integration-simpson-rule/Program.fs
@@ -1,2 +1,19 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+let simpsonRule (f: float -> float) (a: float) (b: float) (n: int) =
+    let h = (b - a) / float n
+    let sum_odd = 
+        seq { for i in 1 .. n/2 -> a + 2.0 * float i * h }
+        |> Seq.sumBy (fun xi -> f xi)
+    let sum_even = 
+        seq { for i in 1 .. n/2-1 -> a + (2.0 * float i - 1.0) * h }
+        |> Seq.sumBy (fun xi -> f xi)
+    (h / 3.0) * (f a + 4.0 * sum_odd + 2.0 * sum_even + f b)
+
+let f x = sin x
+let a = 0.0
+let b = 1.0
+let n = 100
+let integral = simpsonRule f a b n
+
+printfn "The numerical integral of sin(x) from %f to %f using Simpson's rule is approximately %f" a b integral
