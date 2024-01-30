8c5dd4cb8975b4a18f128cff9e3250aceeb69fc1
Jonas Lara <adrianlr2515@outlook.com>
2024-01-29 23:55:40 -0600

    Up

--- src/Numerical-integration-monte-carlo/Program.fs
+++ src/Numerical-integration-monte-carlo/Program.fs
@@ -1,2 +1,23 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+let monteCarloIntegration (f: float -> float) (a: float) (b: float) (n: int) =
+    let rand = System.Random()
+    let mutable sum = 0.0
+
+    for _ in 1 .. n do
+        let x = a + (b - a) * rand.NextDouble()
+        sum <- sum + f x
+
+    let average = sum / float n
+    let integral = average * (b - a)
+    integral
+
+// Ejemplo de uso
+let f x = sin x
+let a = 0.0
+let b = 1.0
+let n = 1000000 // 
+
+let integral = monteCarloIntegration f a b n
+
+printfn "The numerical integral of sin(x) from %f to %f using Monte Carlo method is approximately %f" a b integral
