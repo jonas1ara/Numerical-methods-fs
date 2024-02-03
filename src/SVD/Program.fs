77361389a82a8c7b6f1db5c37270e3c14831121b
Jonas Lara <adrianlr2515@outlook.com>
2024-02-02 21:16:54 -0600

    Added SVD

--- src/SVD/Program.fs
+++ src/SVD/Program.fs
@@ -1,15 +1,13 @@
 ﻿open System
 
-// Función para imprimir una matriz
 let printMatrix (matrix: float[][]) =
     for row in matrix do
         printfn "%A" row
 
-// Función para imprimir un vector
 let printVector (vector: float[]) =
     printfn "%A" vector
 
-// Función para realizar la transposición de una matriz
+// Transpose of a matrix
 let transpose (matrix: float[][]) =
     let numRows = matrix.Length
     let numCols = matrix.[0].Length
@@ -20,7 +18,7 @@ let transpose (matrix: float[][]) =
             result.[i].[j] <- matrix.[j].[i]
     result
 
-// Función para multiplicar dos matrices
+// Multiplication of two matrices
 let multiplyMatrices (matrixA: float[][]) (matrixB: float[][]) =
     let numRowsA = matrixA.Length
     let numColsA = matrixA.[0].Length
@@ -35,45 +33,41 @@ let multiplyMatrices (matrixA: float[][]) (matrixB: float[][]) =
             result.[i].[j] <- sum
     result
 
-// Función para realizar la descomposición en valores singulares (SVD) utilizando la técnica de potencia iterativa
+// Singular Value Decomposition
 let svdDecomposition (matrix: float[][]) =
     let m = matrix.Length
     let n = matrix.[0].Length
 
-    // Paso 1: Multiplicar la matriz original por su transpuesta
+    // Step 1: Calculate mtm matrix
     let mtm = multiplyMatrices (transpose matrix) matrix
 
-    // Paso 2: Calcular los vectores y valores propios de mtm
-    // Aquí se puede usar cualquier algoritmo de valores propios
-    // En este ejemplo, usaremos un algoritmo de potencia iterativa
-    let mutable v = Array.init n (fun _ -> 1.0) // Aproximación inicial de v
-    let mutable u = Array.init m (fun _ -> Array.init m (fun _ -> 0.0)) // Inicializar matriz u como una matriz nula
+    // Step 2: Calculate mtm vectors and eigenvalues
+    // Any eigenvalue algorithm can be used here
+    // In this example, we'll use an iterative power algorithm
+    let mutable v = Array.init n (fun _ -> 1.0)
+    let mutable u = Array.init m (fun _ -> Array.init m (fun _ -> 0.0)) 
 
     let maxIterations = 1000
     let mutable iter = 0
     let mutable error = 1e-10
 
     while iter < maxIterations && error > 1e-10 do
-        let vNew = multiplyMatrices mtm [|v|] |> Array.concat // Calcular mtm * v
-        let sigma = Array.sumBy (fun x -> x * x) vNew |> sqrt // Calcular el valor singular
-        v <- vNew |> Array.map (fun x -> x / sigma) // Normalizar v
+        let vNew = multiplyMatrices mtm [|v|] |> Array.concat // Calculate mtm * v
+        let sigma = Array.sumBy (fun x -> x * x) vNew |> sqrt // Calculate sigma
+        v <- vNew |> Array.map (fun x -> x / sigma) // Normalize v
 
-        // Calcular la aproximación de u
         let uNew = multiplyMatrices matrix [|v|] |> Array.concat
         u <- uNew
 
-        // Calcular el error relativo entre v y vNew
         error <- Array.map2 (fun x y -> abs (x - y)) v vNew |> Array.max
         iter <- iter + 1
 
-    // Paso 3: Calcular la matriz de valores singulares (W)
     let w = Array2D.zeroCreate<float>(m, n)
     for i in 0 .. min m n - 1 do
         w.[i, i] <- v.[i]
 
     u, w, transpose [|v|]
 
-// Ejemplo de uso
 let matrix = [|[|1.0; 2.0; 3.0|]; [|4.0; 5.0; 6.0|]|]
 let u, w, vt = svdDecomposition matrix
 
