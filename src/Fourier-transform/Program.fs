5a6a197ad9d72bff41fd7c1789294ae2a516c7df
Jonas Lara <adrianlr2515@outlook.com>
2024-02-05 21:32:22 -0600

    Added Fourier-transform

--- src/Fourier-transform/Program.fs
+++ src/Fourier-transform/Program.fs
@@ -1,2 +1,30 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.Numerics
+
+// Function to calculate the Discrete Fourier Transform (DFT) of an array of complex numbers
+
+let discreteFourierTransform (input: Complex[]) =
+    let n = input.Length
+    let pi = 4.0 * atan 1.0
+    Array.init n (fun k ->
+        let mutable sum = Complex.Zero
+        for j in 0 .. n - 1 do
+            let angle = -2.0 * pi * float k * float j / float n
+            let omega = Complex.FromPolarCoordinates(1.0, angle)
+            sum <- sum + input.[j] * omega
+        sum)
+
+// Function to print an array of complex numbers
+let printComplexArray (arr: Complex[]) =
+    for i = 0 to arr.Length - 1 do
+        printfn "%A" arr.[i]
+
+let inputData = [| Complex(1.0, 0.0); Complex(2.0, 0.0); Complex(3.0, 0.0); Complex(4.0, 0.0) |]
+
+printfn "Input Data:"
+printComplexArray inputData
+
+let result = discreteFourierTransform inputData
+
+printfn "Discrete Fourier Transform (DFT):"
+printComplexArray result
