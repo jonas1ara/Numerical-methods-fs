4567617f0269d3f9b9ff52ea6165da155bed70ba
Jonas Lara <adrianlr2515@outlook.com>
2024-02-05 21:17:46 -0600

    Added Monte-carlo-method-for-PI

--- src/Monte-carlo-method-for-PI/Program.fs
+++ src/Monte-carlo-method-for-PI/Program.fs
@@ -1,2 +1,23 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+// Function to estimate the value of π using the Monte Carlo method
+
+let monteCarloPiEstimation (numPoints: int) =
+    let rand = System.Random()
+    let mutable numPointsInsideCircle = 0
+
+    for _ in 1 .. numPoints do
+        let x = rand.NextDouble()
+        let y = rand.NextDouble()
+        if x * x + y * y <= 1.0 then
+            numPointsInsideCircle <- numPointsInsideCircle + 1
+
+    let piEstimation = 4.0 * float numPointsInsideCircle / float numPoints
+    piEstimation
+
+// Calculate the value of π using the Monte Carlo method with 1,000,000 points
+
+let numPoints = 1000000
+let estimatedPi = monteCarloPiEstimation numPoints
+
+printfn "The estimated value of π using the Monte Carlo method with %d points is approximately %f" numPoints estimatedPi
