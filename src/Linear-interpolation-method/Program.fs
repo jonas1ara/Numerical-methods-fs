b7c67fc25e388b7439e4e3ce00be2247502b7e49
Jonas Lara <adrianlr2515@outlook.com>
2024-02-05 21:08:52 -0600

    Added Linear-interpolation-method

--- src/Linear-interpolation-method/Program.fs
+++ src/Linear-interpolation-method/Program.fs
@@ -1,2 +1,23 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+// Linear interpolation method
+let linearInterpolation (xValues: float[]) (yValues: float[]) (x: float) =
+    let rec findSegment i =
+        if i = xValues.Length - 1 || xValues.[i + 1] >= x then i
+        else findSegment (i + 1)
+    
+    let segmentIndex = findSegment 0
+    let x0, y0 = xValues.[segmentIndex], yValues.[segmentIndex]
+    let x1, y1 = xValues.[segmentIndex + 1], yValues.[segmentIndex + 1]
+    
+    let slope = (y1 - y0) / (x1 - x0)
+    let intercept = y0 - slope * x0
+    slope * x + intercept
+
+let xValues = [|1.0; 2.0; 3.0; 4.0; 5.0|]
+let yValues = [|2.0; 3.0; 5.0; 7.0; 11.0|]
+let x = 2.5
+
+let y = linearInterpolation xValues yValues x
+
+printfn "Interpolated value at x = %f is y = %f" x y
